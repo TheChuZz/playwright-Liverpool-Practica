de0ab68c6c738142cb913ece1a3c6bd1b6021741
Ruben Arellano <ruben_arellano1@outlook.es>
2026-09-19 16:34:25 -0600

    FIX LOCATOR

--- tests/page/MainPage.ts
+++ tests/page/MainPage.ts
@@ -7,13 +7,17 @@ export class MainPage {
 
     constructor(page: Page) {
         this.page = page;
-        this.titlePoduct = page.locator('plp-page-heading-title-title').first();
-        this.searchBar = page.getByTestId('blt26617d4f2e17657d-header-search-input').first();
+        this.searchBar = page.getByPlaceholder(/Buscar por producto/).filter({ visible: true });
+        this.titlePoduct = page.getByTestId('plp-page-heading-title-title');
     }
+
+    
     async goto() {
         await this.page.goto('https://www.liverpool.com.mx/tienda/home');
     }
+
     async searchForProduct(productName: string) {
+    
         await this.searchBar.fill(productName);
         await this.searchBar.press('Enter');
         await this.titlePoduct.waitFor({ state: 'visible' });
--- tests/specs/MainPage.spec.ts
+++ tests/specs/MainPage.spec.ts
@@ -1,11 +1,10 @@
-import {test} from '@playwright/test';
-import {MainPage} from '../page/MainPage';
+import { test } from '@playwright/test';
+import { MainPage } from '../page/MainPage';
 
-test.describe('Search in main page a product', () =>{
-    test('Enter the text into the search field and validate the result', async ({page}) => {
+test.describe('Search in main page a product', () => {
+    test('Enter the text into the search field and validate the result', async ({ page }) => {
         const mainPage = new MainPage(page);
         await mainPage.goto();
         await mainPage.searchForProduct('Play Station 5');
-
-});
+    });
 });
